2083de3e870d50f5b4cd34861d748089d51c550f
Micah Teeuws <tutmirage@gmail.com>
2021-10-25 12:42:02 +0300

    class StringDataItem

--- lib/dartex.dart
+++ lib/dartex.dart
@@ -5,3 +5,16 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+class StringIdItem {
+  int stringDataOff; //uint
+
+  StringIdItem({required this.stringDataOff});
+}
+
+class StringDataItem {
+  int utf16Size; //uleb128
+  int data; //ubyte[]
+
+  StringDataItem({required this.utf16Size, required this.data});
+}
